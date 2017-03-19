c49fda9acf8fdafd742bab97ef3781d6e3825a60
Mike Field-May <mfieldmay@gmail.com>
2017-03-19 22:06:10 +0000

    Update the Read me

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -14,47 +14,27 @@ class GildedRose
   end
 
   def update_quality()
-    @items.each do |item|
-      #  Sulfuras
-      if item.name == "Sulfuras, Hand of Ragnaros"
-        # Aged Brie
-      elsif item.name == "Aged Brie"
-        aged_brie(item)
-        # backstage pass
-      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-        if item.sell_in < SELL_BY_DATE
-          item.quality = 0
-          item.sell_in -= 1
-        elsif item.quality < MAX_QUALITY && item.sell_in <= CONCERT_IMMINENT
-          item.quality += 3
-          item.sell_in -= 1
-        elsif item.quality < MAX_QUALITY && item.sell_in <= CONCERT_APPROACHING
-          item.quality += 2
-          item.sell_in -= 1
-        elsif item.quality < MAX_QUALITY && item.sell_in >= CONCERT_APPROACHING
-          item.quality += 1
-          item.sell_in -= 1
-        elsif item.sell_in < SELL_BY_DATE
-          item.sell_in = 0
-        end
+    items.each do |item|
+      if special_item?(item)
+        special_method(item)
       else
-        if item.quality > MIN_QUALITY && item.sell_in > SELL_BY_DATE
-          item.quality -= 1
-          item.sell_in -= 1
-        elsif item.quality > MIN_QUALITY && item.sell_in <= SELL_BY_DATE
-          item.quality -= 2
-          item.sell_in -= 1
-        else
-          item.quality = 0
-        end
+        normal_item(item)
       end
     end
   end
 
   private
 
+  attr_reader :special_items, :items
+
   def special_item?(item)
-    special_items.include?(item)
+    special_items.include?(item.name)
+  end
+
+  def special_method(item)
+    item
+    aged_brie(item) if item.name == "Aged Brie"
+    backstage_pass(item) if item.name == "Backstage passes to a TAFKAL80ETC concert"
   end
 
   def aged_brie(item)
@@ -63,6 +43,36 @@ class GildedRose
       item.sell_in -= 1
     else
       item.sell_in -= 1
+    end
+  end
+
+  def backstage_pass(item)
+    if item.sell_in < SELL_BY_DATE
+      item.quality = 0
+      item.sell_in -= 1
+    elsif item.quality < MAX_QUALITY && item.sell_in <= CONCERT_IMMINENT
+      item.quality += 3
+      item.sell_in -= 1
+    elsif item.quality < MAX_QUALITY && item.sell_in <= CONCERT_APPROACHING
+      item.quality += 2
+      item.sell_in -= 1
+    elsif item.quality < MAX_QUALITY && item.sell_in >= CONCERT_APPROACHING
+      item.quality += 1
+      item.sell_in -= 1
+    elsif item.sell_in < SELL_BY_DATE
+      item.sell_in = 0
+    end
+  end
+
+  def normal_item(item)
+    if item.quality > MIN_QUALITY && item.sell_in > SELL_BY_DATE
+      item.quality -= 1
+      item.sell_in -= 1
+    elsif item.quality > MIN_QUALITY && item.sell_in <= SELL_BY_DATE
+      item.quality -= 2
+      item.sell_in -= 1
+    else
+      item.quality = 0
     end
   end
 
